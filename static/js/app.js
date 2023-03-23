fc51c4f5c31fe18680f24ccc34d8d53a70df0619
楊思婷 <yangsiting@christines-MacBook-Air.local>
2023-03-23 16:26:43 -0700

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,95 @@
+const resource="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+
+function init(){
+    d3.json(resource).then((data)=>{
+    let names = data['names']
+    let init_id = names[0]
+    console.log(init_id)
+    utobar(init_id);
+    bubblechart(init_id)
+    metadata_info(init_id)
+    })
+}
+
+
+// create menu option
+data.then(function(data) {
+    let menu = d3.select("#selDataset")
+    let names = data['names'];
+    names.forEach(function(id) {
+        menu.append('option').text(id).property("value", id)});
+    let sample = names[0]
+    console.log(sample)});
+   
+function utobar(sample_id){
+    d3.json(resource).then((data)=>{
+        let samples = data['samples'];
+        let target_sample = samples.filter(x=>x.id==sample_id);
+        console.log(target_sample)
+        let sample_values = target_sample[0]['sample_values'];
+        sample_values = sample_values.slice(0, 10).reverse()
+        console.log(sample_values)
+        let otu_ids = target_sample[0]['otu_ids'];
+        otu_ids = otu_ids.slice(0, 10).reverse().map(id=>`OTU ${id}`)
+        console.log(otu_ids)
+        let otu_labels = target_sample[0]['otu_labels'];
+        otu_labels = otu_labels.slice(0, 10).reverse()
+        let trace = {"x":sample_values, "y":otu_ids, "text":otu_labels, type:"bar","orientation":"h"}
+        let trace_data = [trace]
+        let layout = {title: "Top 10 OTUs Present"}
+        Plotly.newPlot('bar', trace_data, layout)
+    })}
+
+
+function bubblechart(sample_id){
+    d3.json(resource).then((data)=>{
+        let samples = data['samples'];
+        let target_sample = samples.filter(x=>x.id==sample_id);
+        console.log(target_sample)
+        let sample_values = target_sample[0]['sample_values'];
+        console.log(sample_values)
+        let otu_ids = target_sample[0]['otu_ids'];
+        console.log(otu_ids)
+        let otu_labels = target_sample[0]['otu_labels'];
+        var desired_maximum_marker_size = 110;
+        let trace = {"x":otu_ids, "y":sample_values, "type": 'scatter',
+            "mode": 'markers', "text":otu_labels,
+            "marker": {"color":otu_ids, colorscale: "Earth", "size":sample_values, 
+            sizeref: 2.0 * Math.max(...sample_values) / (desired_maximum_marker_size**2),
+            sizemode: 'area'}}
+        let trace_data = [trace]
+        let layout = {xaxis:{title:{text:"OTU ID"}}}
+        Plotly.newPlot('bubble', trace_data, layout)
+    })
+    }
+
+function metadata_info(sample_id){
+    d3.json(resource).then((data)=>{
+        let metadata = data['metadata']
+        let target_metadata = metadata.filter((x=>x.id == sample_id))
+        console.log(target_metadata)
+        let metadata_value =  target_metadata[0]
+        let meta_keys = Object.keys(metadata_value)
+        console.log(meta_keys)
+        let meta_values = Object.values(metadata_value)
+        console.log(meta_values)
+        d3.select("#sample-metadata").html("")
+        for (let i=0; i<meta_keys.length; i++){
+            d3.select("#sample-metadata").append("h5").text(`${meta_keys[i]}: ${meta_values[i]}`)}
+        })
+    }
+
+//onchange="optionChanged(this.value)"
+function optionChanged(selectObject){
+    console.log(selectObject);
+    utobar(selectObject);
+    bubblechart(selectObject)
+    metadata_info(selectObject)
+};
+
+init()
+
+
+
+
---
+++ static/js/bonus.js
@@ -0,0 +1,72 @@
+
+let datas = d3.json(resource)
+datas.then(function(data) {
+    let names = data['names'];
+    names.forEach(function(id) {
+        menu.append('option').text(id).property("value", id)});
+    let sample = names[0]
+    console.log(sample)});
+
+function init(){
+    d3.json(resource).then((data)=>{
+    let names = data['names']
+    let init_id = names[0]
+    console.log(init_id)
+    gauge(init_id)})}
+
+function gauge(id){
+    datas.then((data)=>{
+        let meta=data['metadata']
+        let target_meta = meta.filter((id_value)=>id_value.id==id)
+        console.log(target_meta)
+        let meta_value = target_meta[0]
+        console.log(meta_value)
+        let wfreq = meta_value['wfreq']
+        console.log(wfreq)
+    
+
+        let graph = {
+            value: wfreq,
+            domain: {x: [0,1], y: [0,1]},
+            title: {
+                text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+                font: {color: "black", size: 18}
+            },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+            axis: {range: [0,10], tickmode: "linear", tick0: 1, dtick: 1},
+            bar: {color: "rgba(208, 150, 210, 0.86)"},
+            steps: [
+                {range: [0, 1], color: "rgba(245, 39, 145, 0.02)"},
+                {range: [1, 2], color: "rgba(245, 39, 145, 0.07)"},
+                {range: [2, 3], color: "rgba(245, 39, 145, 0.12)"},
+                {range: [3, 4], color:  "rgba(245, 39, 145, .17)"},
+                {range: [4, 5], color:  "rgba(245, 39, 145, .22)"},
+                {range: [5, 6], color: "rgba(245, 39, 145, .27)"},
+                {range: [6, 7], color: "rgba(245, 39, 145, .32)"},
+                {range: [7, 8], color:  "rgba(245, 39, 145, .37)"},
+                {range: [8, 9], color: "rgba(245, 39, 145, 0.42)"},
+                {range: [9, 10], color: "rgba(245, 39, 145, .47)"},
+            ]
+            } 
+        };
+
+// Set up the Layout
+        let layout = {
+    width: 425, 
+    height: 425,
+    margin: {t: 0, b:0}
+        };
+
+// Call Plotly to plot the gauge chart
+        Plotly.newPlot("gauge", [graph], layout)
+    })}
+
+    function optionChanged(selectObject){
+        gauge(selectObject)}
+        
+ /// var dataGauge = [traceGauge]
+
+init()
+ /// Plotly.plot('gauge', dataGauge, gaugeLayout)
